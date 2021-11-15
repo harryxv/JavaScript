62ee8c6da951ed9ba7a861ab53eea552bdea1852
admin <admin@Harrys-MacBook-Air.local>
2021-11-14 19:46:11 -0500

    change name

--- algorithm/src/test/js/BinarySearchTest.js
+++
@@ -1,4 +0,0 @@
-import {binarySearch} from "../../main/js/BinarySearch.js";
-
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-console.log("binarySearch(5, array) = " + binarySearch(5, array));
